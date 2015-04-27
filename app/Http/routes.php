7dc287a4a36ce393079c375939991f541cef463d
Sandy Davidson <sandy@sandysandy.net>
2015-04-27 20:16:06 +0100

    The routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,38 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+# app/Http/routes.php
 
+use App\Models\Quote;
+
+/**
+ * Display the today quote
+ */
 $app->get('/', function() use ($app) {
-    return $app->welcome();
+
+    /*
+     * Picks a different quote every day 
+     * (for a maximum of 366 quotes)
+     *
+     *   - $count: the total number of available quotes
+     *   - $day: the current day of the year (from 0 to 365)
+     *   - $page: the page to look for to retrieve the 
+     *            correct record
+     */
+    $count = Quote::query()->get()->count();
+    $day = (int) date('z');
+    $page = $day % $count + 1;
+
+    $quotes = Quote::query()->get()->forPage($page, 1)->all();
+
+    if (empty($quotes)) {
+        throw new \Illuminate\Database\Eloquent\ModelNotFoundException();
+    }
+
+    return view('quote', ['quote' => $quotes[0]]);
 });
+
+/**
+ * Display a specific quote
+ */
+$app->get('/{id}', function($id) use ($app) {
+    $quote = Quote::query()->findOrFail($id);
